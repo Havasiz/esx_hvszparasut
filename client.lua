fd7cc2d4c4d4db8cf0afc34e9f8156af0327965d
Havasiz <61249972+Havasiz@users.noreply.github.com>
2021-11-02 18:47:02 +0100

    Update client.lua

--- client.lua
+++ client.lua
@@ -10,7 +10,7 @@ end)
 
 function StartParaCheck() 
 	print("start")
-	SetPedComponentVariation(PlayerPedId(), 5, 7, 0, 1) --Paraşüt itemini kullandığınızda sırtınızda spawn olan paraşüt (İsterseniz değiştirebilirsiniz)
+	SetPedComponentVariation(PlayerPedId(), 5, 7, 0, 1) --Paraşüt itemini kullandığınızda sırtınızda spawn olan paraşüt (İsterseniz değiştirebilirsiniz) / The model of the Parachute that spawns on your back when you use the item "Parachute" (You can change the model).
 	Citizen.CreateThread(function()
 		local ped = PlayerPedId()
 
@@ -34,7 +34,7 @@ function StartLandCheck()
 			local state = GetPedParachuteState(ped)
 
 			if state == -1 then
-				SetPedComponentVariation(ped, 5, 0, 0, 0) --Yere iniş gerçekleştirdikten sonra sırtınızdaki paraşütün yok olması, isterseniz yok olduktan sonra çanta v.b. spawn ettirebilirsiniz burakadi kodlari değiştirerek.
+				SetPedComponentVariation(ped, 5, 0, 0, 0) --Yere iniş gerçekleştirdikten sonra sırtınızdaki paraşütün yok olması, isterseniz yok olduktan sonra çanta v.b. spawn ettirebilirsiniz burakadi kodlari değiştirerek. / After landing, normally I made it so the parachute dissapears but you can make it so after the landing the parachute gets replaced with a "bag" or whatever.
 				return
 			end
 			Citizen.Wait(1)
