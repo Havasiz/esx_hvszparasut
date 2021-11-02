314a4a9263c53296de3775fa7264fa58952e0e2a
Havasiz <61249972+Havasiz@users.noreply.github.com>
2021-11-02 18:50:36 +0100

    Added English translation.

--- server.lua
+++ server.lua
@@ -7,7 +7,7 @@ TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
 ESX.RegisterUsableItem('parasut', function(source)
 	local _source = source
     local xPlayer = ESX.GetPlayerFromId(_source)
-    local parasut = xPlayer.getInventoryItem('parasut')
+    local parasut = xPlayer.getInventoryItem('parasut') -- Database item adı. / Database item name.
 
 	if parasut.count > 0 then
 		xPlayer.removeInventoryItem('parasut', 1)
